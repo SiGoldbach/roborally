24fba731107a34bc35ba49741b4aa78e2b859df9
Sebastian Goldbach <Sig.goldbach@gmail.com>
2022-04-14 21:18:08 +0200

    A bit off javadoc commenting

--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Checkpoint.java
@@ -4,12 +4,22 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class Checkpoint extends FieldAction{
     private final int checkPointsRequired;
+
+    /**
+     * This constructor creates this action the checkpoints have to be ordered
+     * @param checkPointsRequired
+     */
     public Checkpoint(int checkPointsRequired){
         this.checkPointsRequired=checkPointsRequired;
 
     }
 
-
+    /**
+     * Here it increments the players checkpoint amount to control if the player can get the checkpoint yet
+     * @param gameController the gameController of the respective game
+     * @param space the space this action should be executed for
+     * @return
+     */
     public  boolean doAction(GameController gameController, Space space){
         if(space.getPlayer()==null)
             return false;
--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FinishLine.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FinishLine.java
@@ -5,7 +5,7 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 public class FinishLine extends FieldAction{
     /**
      * Here is a method for determining if the player on the victory fields has all checkpoints
-     * if he has he should win
+     * if he has he should win. It does not do anything yet since it needs some method to call but there is not any.
      * @param gameController the gameController of the respective game
      * @param space the space this action should be executed for
      * @return
