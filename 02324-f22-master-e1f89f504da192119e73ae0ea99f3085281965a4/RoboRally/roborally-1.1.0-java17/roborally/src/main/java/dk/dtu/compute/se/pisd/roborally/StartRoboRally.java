6b30285d5ef9667b25aa4de151e43215e80ee250
Sebastian <Sig.goldbach@gmail.com>
2022-06-06 17:55:11 +0200

    New class for controllling server client relationship and making get requests and so on

--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/StartRoboRally.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/StartRoboRally.java
@@ -21,6 +21,8 @@
  */
 package dk.dtu.compute.se.pisd.roborally;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * This is a class for starting up the RoboRally application. This is a
  * workaround for a strange quirk in the Open JavaFX project launcher,
@@ -30,6 +32,7 @@ package dk.dtu.compute.se.pisd.roborally;
  *
  * @author Ekkart Kindler, ekki@dtu.dk
  */
+@SpringBootApplication
 public class StartRoboRally {
 
     public static void main(String[] args) {
---
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/ServerClientController.java
@@ -0,0 +1,21 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerClientController {
+
+
+    public String getBoard(String boardName){
+
+
+        return null;
+
+    }
+
+
+
+
+
+
+}
