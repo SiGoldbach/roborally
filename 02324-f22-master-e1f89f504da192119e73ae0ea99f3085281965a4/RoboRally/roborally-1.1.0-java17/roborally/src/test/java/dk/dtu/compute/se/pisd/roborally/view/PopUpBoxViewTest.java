e48c07e1ce0a58d01a40488a49507136c5bd5310
Sebastian Goldbach <Sig.goldbach@gmail.com>
2022-06-07 20:33:10 +0200

    Testing method

--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/view/PopUpBoxViewTest.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/view/PopUpBoxViewTest.java
@@ -1,15 +1,16 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 
+import dk.dtu.compute.se.pisd.roborally.controller.ServerClientController;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 public class PopUpBoxViewTest {
 
     @Test
-    void loadGameTest(){
+    void loadGameTest() {
         List<String> games = new ArrayList<>();
         games.add("awdnin");
         games.add("jaja");
@@ -18,7 +19,18 @@ public class PopUpBoxViewTest {
 
     }
 
+    /**
+     * Testing two get requests and the popupBox
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Test
+    void testOfGetBoardsAndMenuAndGetSpecificBoard() throws IOException, InterruptedException {
+        String val = new PopUpBoxView().loadGame(new ServerClientController().possibleBoards());
+        String board = new ServerClientController().getBoard(val);
+        System.out.println(board);
 
+    }
 
 
 }
