d2a5e23eb899588868d4331007c85fb010af90b5
Sebastian Goldbach <Sig.goldbach@gmail.com>
2022-06-07 14:33:52 +0200

    Correct get for the boards

--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -77,18 +77,17 @@ public class AppController extends PopUpBoxView implements Observer {
 
             // XXX the board should eventually be created programmatically or loaded from a file
             //     here we just create an empty board with the required number of players.
-            Board board = new Board(10, 10);
-            Board myBoard=LoadBoard.loadBoard("defaultboard");
-            if(board==null){
-                System.out.println("Error");
+            Board myBoard=LoadBoard.loadBoard(new PopUpBoxView().gameInstance("Enter file","Game loaded"));
+            if(myBoard==null){
+                System.out.println("Getting default board");
+                myBoard=LoadBoard.loadBoard("defaultboard");
             }
-            gameController = new GameController(board);
-            gameController.board.setWallsTest();
+            gameController = new GameController(myBoard);
             int no = result.get();
             for (int i = 0; i < no; i++) {
-                Player player = new Player(board, PLAYER_COLORS.get(i), "Player " + (i + 1));
-                board.addPlayer(player);
-                player.setSpace(board.getSpace(i % board.width, i));
+                Player player = new Player(gameController.board, PLAYER_COLORS.get(i), "Player " + (i + 1));
+                gameController.board.addPlayer(player);
+                player.setSpace(gameController.board.getSpace(i % gameController.board.width, i));
             }
 
             // XXX: V2
