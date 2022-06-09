5c0988c15c0476bfbc00597a9b750c63b8a7174f
KaNN <lasseg123@hotmail.com>
2022-06-09 19:01:22 +0200

    Missing while

--- 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ 02324-f22-master-e1f89f504da192119e73ae0ea99f3085281965a4/RoboRally/roborally-1.1.0-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -123,6 +123,7 @@ public class GameController {
                     System.out.println("BIGWAITTIME");
                 } else {
                     startProgrammingPhase();
+                    break;
                 }
             }
         });
